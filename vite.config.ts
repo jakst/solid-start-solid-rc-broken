6f9bf8d7ec11ebffd3c75ab8f9bbfef56a23b9c6
Jakob Norlin <jakob.stahl91@gmail.com>
2025-09-29 10:18:58 +0200

    Set up Cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { cloudflare } from "@cloudflare/vite-plugin"
 import { tanstackStart } from "@tanstack/solid-start/plugin/vite"
 import { defineConfig } from "vite"
 import viteSolid from "vite-plugin-solid"
@@ -6,5 +7,9 @@ export default defineConfig({
 	server: {
 		port: 3000,
 	},
-	plugins: [tanstackStart(), viteSolid({ ssr: true })],
+	plugins: [
+		cloudflare({ viteEnvironment: { name: "ssr" } }) as any,
+		tanstackStart(),
+		viteSolid({ ssr: true }),
+	],
 })
